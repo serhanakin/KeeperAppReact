59310bd8db247929a2b7665c8d2e1e9edac0828f
Serhan AKIN <serhanakin.py@gmail.com>
2022-12-19 23:15:20 +0300

    CreateNote fonksiyonu eklendi.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import './App.css';
 import Header from './View/Header';
 import Footer from './View/Footer';
 import Note from './View/Note';
+import notes from './Notes';
+
+function createNotes(note) {
+  return <Note 
+    key={note.key}
+    title={note.title}
+    content={note.content}
+  />
+}
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map(createNotes)}
       <Footer />
     </div>
   );
